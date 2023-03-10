200733a3b85fbd7fdb66aa9966ff4098635ffdbb
Param <bittusmaths@gmail.com>
2023-03-10 08:48:14 +0530

    Update product.ts

--- schemas/product.ts
+++ schemas/product.ts
@@ -65,8 +65,6 @@ export default
                   { title: 'H2', value: 'h2' },
                   { title: 'H3', value: 'h3' },
                   { title: 'H4', value: 'h4' },
-                  { title: 'H5', value: 'h5' },
-                  { title: 'H6', value: 'h6' },
                   { title: 'Quote', value: 'blockquote' }
                 ],
                 // Only allow numbered lists
