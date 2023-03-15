ba40df5c8291329904e21ab91aacbb840e6840cf
Cody <bittusmaths@gmail.com>
2023-03-15 18:36:24 +0530

    Removed categories :joy:

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,5 @@
 // schemas/index.js
 import product from './product'
 import slider from './slider'
-import categories from './categories'
 
-export const schemaTypes = [product, slider, categories]
+export const schemaTypes = [product, slider]
